330d77ed13d452802a5e4d3ac3f778b7613859c2
Md. Shamim Rahman <apubdtea@gmail.com>
2024-07-14 19:45:20 +0600

    feat: define models for the polls app

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    """Define a model for the questions."""
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    """A choice model for the question."""
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
