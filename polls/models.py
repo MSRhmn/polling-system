79dc446f8c15f30ebc22a73c240300c97d1d729c
Md. Shamim Rahman <apubdtea@gmail.com>
2025-04-12 11:29:28 +0600

    feat: add str method models

--- polls/models.py
+++ polls/models.py
@@ -3,13 +3,20 @@ from django.db import models
 
 class Question(models.Model):
     """Define a model for the questions."""
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
     """A choice model for the question."""
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
